fb83681b59049f2724ceb254b562aabc5cbecb83
jean-luc-twite <83658859+jean-luc-twite@users.noreply.github.com>
2024-03-25 14:36:48 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, this is a Flask app running in AWS!'
+    return 'Hello, this is the first deployment test 1 !'
 
 if __name__ == '__main__':
     app.run(port=5000, host='0.0.0.0')
