a789e272593600265fba9b7daeaea883a911e272
jean-luc-twite <83658859+jean-luc-twite@users.noreply.github.com>
2024-03-25 15:15:12 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, this is the first deployment from the best dev team of fluid intellect'
+    return 'Hello, this is the first deployment from the best dev team of fluid intellect test1'
 
 if __name__ == '__main__':
     app.run(port=5000, host='0.0.0.0')
