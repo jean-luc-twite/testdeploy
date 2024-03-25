619c223e3da9b99845ca627749d5e2a08e7fd7d3
jean-luc-twite <83658859+jean-luc-twite@users.noreply.github.com>
2024-03-25 14:52:20 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, this is the first deployment test!'
+    return 'Hello, this is the first deployment test 2'
 
 if __name__ == '__main__':
     app.run(port=5000, host='0.0.0.0')
